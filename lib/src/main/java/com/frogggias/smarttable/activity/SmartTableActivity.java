ec0c0d85657c2477a8445d40526b1b2d0c1a78ec
Maroš Šeleng <marosseleng@gmail.com>
2017-02-02 15:06:05 +0100

    Remove unused import.

--- lib/src/main/java/com/frogggias/smarttable/activity/SmartTableActivity.java
+++ lib/src/main/java/com/frogggias/smarttable/activity/SmartTableActivity.java
@@ -11,7 +11,6 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
