aa3b38ab61032927c91ac01d57cb6b12ff90cce9
Maroš Šeleng <marosseleng@gmail.com>
2019-03-07 16:03:28 +0100

    Remove forgotten FileProvider.

--- lib/src/main/java/com/frogggias/smarttable/provider/SmartTableFileProvider.java
+++
@@ -1,9 +0,0 @@
-package com.frogggias.smarttable.provider;
-
-import android.support.v4.content.FileProvider;
-
-/**
- * @author Maroš Šeleng
- */
-public class SmartTableFileProvider extends FileProvider {
-}
